65eac34a9ea8b78f59ed5256deda8ee31e29ce2c
kehayov <tihomirkehayov@deltasource.eu>
2018-01-14 18:33:36 +0200

    delete product; implement dialogs

--- src/app/theme/products/products.component.ts
+++ src/app/theme/products/products.component.ts
@@ -4,22 +4,70 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CustomValidators } from 'ng2-validation';
 import { BASICENDPOINT } from '../../constants';
+import { transition, trigger, style, animate } from '@angular/animations';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: [
     './products.component.scss',
-    './../../../assets/icon/icofont/css/icofont.scss']
+    './../../../assets/icon/icofont/css/icofont.scss',
+    '../../../../node_modules/sweetalert2/src/sweetalert2.scss'
 })
+
 export class ProductsComponent implements OnInit {
   products = [];
+  showDialog = false;
+  public config: any;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get(BASICENDPOINT + '/products').subscribe(data => {
       this.products = data;
     });
+  }
+
+  delete() {
+    swal({
+      title: 'Are you sure?',
+      text: 'Product wont be able to revert',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete',
+      cancelButtonText: 'No, cancel',
+      confirmButtonClass: 'btn btn-success m-r-10',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: false
+    }).then((result) => {
+      if (result.value) {
+        // TODO change with dynamic id
+        this.http.delete(BASICENDPOINT + '/products/23').subscribe(data => {
+          swal(
+            'Deleted!',
+            'Product has been deleted.',
+            'success'
+          )
+          
+          this.http.get(BASICENDPOINT + '/products').subscribe(data => {
+            this.products = data;
+          });
+        });
+      } else if (result.dismiss === 'cancel') {
+        swal(
+          'Cancelled',
+          'Product is not deleted',
+          'error'
+        )
+      }
+    }).catch(swal.noop);
+
+  }
+  openSuccessCancelSwal() {
+
   }
 
 }
