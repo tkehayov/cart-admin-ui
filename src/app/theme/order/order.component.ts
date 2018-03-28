a2e74bca187e5df1e5434bff8f0753360c0f0d08
kehayov <tihomirkehayov@deltasource.eu>
2018-03-28 21:49:41 +0300

    implement product in order

--- src/app/theme/order/order.component.ts
+++ src/app/theme/order/order.component.ts
@@ -3,8 +3,8 @@ import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BASICENDPOINT } from '../../constants';
-
 import { NotificationsService } from 'angular2-notifications';
+
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
@@ -15,23 +15,39 @@ import { NotificationsService } from 'angular2-notifications';
 })
 
 export class OrderComponent implements OnInit {
-  params = {};
+  queryParams: { id: "" };
+  order = {};
+  products = [];
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.route.params.subscribe(params => this.params = params);
+    this.route.params.subscribe(params => this.queryParams = params.id);
 
-    this.http.get(BASICENDPOINT + '/orders').subscribe(data => {
+    this.http.get(BASICENDPOINT + '/orders/' + this.queryParams).subscribe(data => {
       var jsonData = JSON.parse(JSON.stringify(data));
-      // this.orders.data = jsonData;
+      this.order = jsonData;
+      this.listProducts();
     });
+  }
+
+  listProducts() {
+    var key;
+    for (key of this.order.productsId) {
+      this.http.get(BASICENDPOINT + '/products/' + key).subscribe(data => {
+        var jsonData = JSON.parse(JSON.stringify(data));
+        this.products.push(data);
+      });
+    }
   }
 
   updateStatus(order, status) {
     this.http.put(BASICENDPOINT + '/orders', { "id": order.id, "status": status }).subscribe(data => {
-      // order.status = status;
     });
+  }
+
+  goProductDetail(productId) {
+    console.log("welcome");
   }
 }
